64e97498efb3a3a129807df89ffe717c8c7b8e40
yuman montero <yuman11@hotmail.com>
2019-09-23 15:28:07 -0300

    -Agregado Restsharp rest api -Agregado Json

---
+++ MosaMosaicIntegration/Modelo/Mensaje/TaquillaActivarRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosaMosaicIntegration.Modelo.Mensaje
+{
+    class TaquillaActivarRequest
+    {
+        public int nroterminal { get; set; }
+        public long codoficina { get; set; }
+        public String carnetatencion { get; set; }
+
+       
+    }
+}
---
+++ MosaMosaicIntegration/Modelo/Mensaje/TaquillaActivarResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosaMosaicIntegration.Modelo.Mensaje
+{
+    class TaquillaActivarResponse
+    {
+        public TaquillaDat taquilla { set; get; }
+        public EstatusDat estatus { set; get; }
+
+
+    }
+}
