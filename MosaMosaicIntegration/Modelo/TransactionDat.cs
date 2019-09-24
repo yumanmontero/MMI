ad257c0264c4f323e3b3a8323770989ee75a95ae
yumanmontero <yumanmontero@gmail.com>
2019-09-23 22:02:57 -0400

    -Mejorado los Rest Request

--- MosaMosaicIntegration/Modelo/TransactionDat.cs
+++ MosaMosaicIntegration/Modelo/TransactionDat.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +9,32 @@ namespace MosaMosaicIntegration.Modelo
 {
     class TransactionDat
     {
-        public Boolean exists;
-        public long nroticket;
-        public String fechaatencion;
-        public String horallegadaofic;
-        public DateTime? horallegadaoficD;
-        public DateTime? fechaatencionD;
-        public String idcedula;
-        public long nrocedula;
-        public DateTime? horafintrx;
-        public long montotrx;
-        public long idtrx;
-        public String codtrx;
-        public long codoficina;
+        [JsonProperty("exists")]
+        public Boolean exists { get; set; }
+        [JsonProperty("nroticket")]
+        public long nroticket { get; set; }
+        [JsonProperty("fechaatencion")]
+        public String fechaatencion { get; set; }
+        [JsonProperty("horallegadaofic")]
+        public String horallegadaofic { get; set; }
+        [JsonProperty("horallegadaoficD")]
+        public DateTime? horallegadaoficD { get; set; }
+        [JsonProperty("fechaatencionD")]
+        public DateTime? fechaatencionD { get; set; }
+        [JsonProperty("idcedula")]
+        public String idcedula { get; set; }
+        [JsonProperty("nrocedula")]
+        public long nrocedula { get; set; }
+        [JsonProperty("horafintrx")]
+        public DateTime? horafintrx { get; set; }
+        [JsonProperty("montotrx")]
+        public long montotrx { get; set; }
+        [JsonProperty("idtrx")]
+        public long idtrx { get; set; }
+        [JsonProperty("codtrx")]
+        public String codtrx { get; set; }
+        [JsonProperty("codoficina")]
+        public long codoficina { get; set; }
 
         public void configFechaHora()
         {
